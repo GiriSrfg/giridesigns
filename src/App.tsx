e3885a26a6f90883ba526f874cc8caa5965a2196
Giri <giri@example.com>
2026-06-26 09:15:37 +0530

    V3

--- src/App.tsx
+++ src/App.tsx
@@ -522,7 +522,7 @@ function ContactSection({ onContactClick }: { onContactClick?: () => void }) {
   return (
     <section
       id="contact"
-      className="relative bg-transparent py-20 sm:py-24 text-white overflow-hidden"
+      className="relative bg-transparent py-14 sm:py-16 text-white overflow-hidden"
     >
       <div className="relative z-10 px-6 sm:px-8 lg:px-[8.5vw]">
         {/* Section Header */}
@@ -1800,21 +1800,21 @@ function App() {
             </div>
           </section>
 
-          <section id='about' className='relative min-h-screen px-5 py-12 sm:py-16 md:px-10'>
+          <section id='about' className='relative min-h-[70vh] md:min-h-screen px-5 py-12 sm:py-16 md:px-10'>
             <FadeIn delay={0.1} x={-80} y={0} duration={0.9}>
-              <img src='https://shrug-person-78902957.figma.site/_components/v2/ebb2b8f25d8e24d5f0a5ca8af4c950de81aa2fd7/moon_icon.11395d36.png' alt='' className='absolute left-[1%] top-[4%] w-[120px] sm:left-[2%] sm:w-[160px] md:left-[4%] md:w-[210px]' />
+              <img src='https://shrug-person-78902957.figma.site/_components/v2/ebb2b8f25d8e24d5f0a5ca8af4c950de81aa2fd7/moon_icon.11395d36.png' alt='' className='absolute left-[1%] top-[8%] sm:top-[6%] md:top-[4%] w-[120px] sm:left-[2%] sm:w-[160px] md:left-[4%] md:w-[210px]' />
             </FadeIn>
             <FadeIn delay={0.25} x={-80} y={0} duration={0.9}>
-              <img src='https://shrug-person-78902957.figma.site/_components/v2/ebb2b8f25d8e24d5f0a5ca8af4c950de81aa2fd7/p59_1.4659672e.png' alt='' className='absolute bottom-[8%] left-[3%] w-[100px] sm:left-[6%] sm:w-[140px] md:left-[10%] md:w-[180px]' />
+              <img src='https://shrug-person-78902957.figma.site/_components/v2/ebb2b8f25d8e24d5f0a5ca8af4c950de81aa2fd7/p59_1.4659672e.png' alt='' className='absolute bottom-[14%] sm:bottom-[11%] md:bottom-[8%] left-[3%] w-[100px] sm:left-[6%] sm:w-[140px] md:left-[10%] md:w-[180px]' />
             </FadeIn>
             <FadeIn delay={0.15} x={80} y={0} duration={0.9}>
-              <img src='https://shrug-person-78902957.figma.site/_components/v2/ebb2b8f25d8e24d5f0a5ca8af4c950de81aa2fd7/lego_icon-1.703bb594.png' alt='' className='absolute right-[1%] top-[4%] w-[120px] sm:right-[2%] sm:w-[160px] md:right-[4%] md:w-[210px]' />
+              <img src='https://shrug-person-78902957.figma.site/_components/v2/ebb2b8f25d8e24d5f0a5ca8af4c950de81aa2fd7/lego_icon-1.703bb594.png' alt='' className='absolute right-[1%] top-[8%] sm:top-[6%] md:top-[4%] w-[120px] sm:right-[2%] sm:w-[160px] md:right-[4%] md:w-[210px]' />
             </FadeIn>
             <FadeIn delay={0.3} x={80} y={0} duration={0.9}>
-              <img src='https://shrug-person-78902957.figma.site/_components/v2/ebb2b8f25d8e24d5f0a5ca8af4c950de81aa2fd7/Group_134-1.2e04f3ce.png' alt='' className='absolute bottom-[8%] right-[3%] w-[130px] sm:right-[6%] sm:w-[170px] md:right-[10%] md:w-[220px]' />
+              <img src='https://shrug-person-78902957.figma.site/_components/v2/ebb2b8f25d8e24d5f0a5ca8af4c950de81aa2fd7/Group_134-1.2e04f3ce.png' alt='' className='absolute bottom-[14%] sm:bottom-[11%] md:bottom-[8%] right-[3%] w-[130px] sm:right-[6%] sm:w-[170px] md:right-[10%] md:w-[220px]' />
             </FadeIn>
 
-            <div className='flex min-h-[65vh] flex-col items-center justify-center gap-8 sm:gap-12 md:gap-14'>
+            <div className='flex min-h-[50vh] md:min-h-[60vh] flex-col items-center justify-center gap-8 sm:gap-12 md:gap-14'>
               <FadeIn delay={0} y={40} duration={0.8}>
                 <h2 className='hero-heading text-center text-[clamp(3rem,12vw,160px)] font-black uppercase leading-none tracking-tight'>About me</h2>
               </FadeIn>
